4b46c1ccc9325dfab5815f26de99fd3c417989f6
Saurabh Charde <saurabhchardereal@gmail.com>
2023-05-18 16:38:54 +0530

    initial template commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const tailwindConfig = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["var(--font-family-sans)", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
+};
+
+export default tailwindConfig;
